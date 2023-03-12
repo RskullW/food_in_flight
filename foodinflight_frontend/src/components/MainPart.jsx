02cd2171a4e46da85b8d4f31941a635629af7f72
Mihalych32 <nikita-mikhaylovsky@mail.ru>
2023-03-12 15:45:42 +0300

    added: MainPart (Content-part) updated

--- foodinflight_frontend/src/components/MainPart.jsx
+++ foodinflight_frontend/src/components/MainPart.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Grid, GridItem, Box, Image, Link} from "@chakra-ui/react";
+import { Grid, GridItem, Box, Image, Link, Heading, Wrap, Center, WrapItem, Text, Button, UnorderedList, ListItem, IconButton} from "@chakra-ui/react";
 import { CATEGORIES } from "../data/Categories";
+import { POPULAR_CATEGORIES } from "../data/PopularCategories";
+import { KITCHENS } from "../data/Kitchens";
 
 const MainPart = () => {
   return (
@@ -8,18 +10,84 @@ const MainPart = () => {
       <Box className="carousel" border="2px solid blue" margin="20px">
         <Image src="/favicon.ico"></Image>
       </Box>
-      <Grid className="category" gridTemplateColumns="repeat(4, 1fr)" gap="25px" border="2px solid blue" margin="20px">
-        {
+      <Box className="category" border="2px solid blue" margin="50px 20px 0px 20px" padding="20px 0px">
+        <Wrap justify="center" margin="20px 0px">
+          {
           CATEGORIES.map((CATEGORIE) => (
-            <GridItem display="block" border="5px solid pink">
+            <WrapItem border="5px solid pink" w="200px">
               <Link style={{textDecoration: "none"}}>{CATEGORIE.title}</Link>
-            </GridItem>
+            </WrapItem>
           ))
         }
+        </Wrap>
+      </Box>
+      <Box className="popular-category" border="2px solid blue" margin="100px 20px 0px 20px">
+        <Center className="popular-category__header" area={`header`}>
+          <Heading as="h2">Популярное</Heading>
+        </Center>
+        <Wrap justify="center" margin="20px 0px">
+          {
+          POPULAR_CATEGORIES.map((POPULAR_CATEGORIE) => (
+            <WrapItem className="popular-category__item" area={`item`} border="5px solid pink" w="250px">
+              <Link style={{textDecoration: "none"}}>{POPULAR_CATEGORIE.title}</Link>
+            </WrapItem>
+          ))
+        }
+        </Wrap>
+      </Box>
+      <Box className="extra-info" border="2px solid blue" margin="20px">
+        <Wrap className="extra-info__image-block" justify="center" border="5px solid pink" margin="20px 0px 0px 0px" padding="20px 0px">
+          <WrapItem>
+            <Image src="/favicon.ico" objectFit="cover"></Image>
+          </WrapItem>
+          <WrapItem>
+            <Text> Во главе каждой из наших кухонь стоят повара, которые живут своей работой, вкладывают душу.</Text>
+          </WrapItem>
+          <WrapItem>
+            <Button>Узнать больше</Button>
+          </WrapItem>
+        </Wrap>
+
+        <Box className="extra-info__article-block" margin="50px 0px" padding="20px 0px">
+          <Heading as="h1" margin="20px 0px 0px 0px">Бесплатная доставка еды 24 часа домой и в офис</Heading>
+          <Text margin="20px 0px 0px 0px">Представьте, что специально для вас трудятся квалифицированные повара. Они замешивают душистое тесто, делают оригинальные соусы. Изысканные десерты, закуски становятся шедеврами в руках настоящих мастеров. Вы можете попробовать новое блюдо или заказать что-то традиционное. Совсем не обязательно искать поваренную книгу, хорошее заведение рядом с домом или работой. Идеальный обед, перекус или ужин гораздо ближе, чем вы думаете.</Text>
+          <Heading as="h2" margin="20px 0px 0px 0px">Заказать еду на дом</Heading>
+          <Text margin="20px 0px 0px 0px">Чем еще мы можем порадовать своих клиентов? Например, круглосуточной доставкой еды по Санкт-Петербургу. Если настигнет голод, мы готовы решить эту проблему с помощью наших логистических и кулинарных возможностей. Мы с удовольствием доставим вам первые и вторые блюда, десерты, суши, снеки и напитки, приготовленные по рецептам русской, итальянской, японской и осетинской кухонь. Кроме традиционных национальных, у наших шеф-поваров есть собственные кулинарные рецепты, которые непременно стоит попробовать.</Text>
+        </Box>
+      </Box>
+      <Grid className="footer" gridTemplateColumns="repeat(3,1fr)" border="2px solid blue" margin="20px">
+        <GridItem className="footer__nav-menu">
+          <Heading>Кухни</Heading>
+          <UnorderedList>
+            {
+              KITCHENS.map((KITCHEN) => (
+                <ListItem>
+                  <Link>{(KITCHEN.title)}</Link>
+                </ListItem>
+              ))
+            }
+          </UnorderedList>
+        </GridItem>
+        <GridItem className="footer__nav-menu">
+          <Heading>Компания</Heading>
+          <UnorderedList>
+            <ListItem><Link>Наша Кухня</Link></ListItem>
+            <ListItem><Link>Оплата</Link></ListItem>
+            <ListItem><Link>Контакты</Link></ListItem>
+          </UnorderedList>
+        </GridItem>
+        <GridItem className="footer__nav-item">
+          <Wrap>
+            <Heading>Заказывайте через мобильное приложение</Heading>
+            <Text>Получай подарок к каждому заказу</Text>
+            <Button className="footer__nav-links-app">Загрузить в App Store</Button>
+            <Button className="footer__nav-links-app">Загрузить в Google Play</Button>
+            <IconButton className="footer__nav-links-socials"></IconButton>
+            <IconButton className="footer__nav-links-socials"></IconButton>
+            <IconButton className="footer__nav-links-socials"></IconButton>
+          </Wrap>
+        </GridItem>
       </Grid>
-      <Grid className="popular-category" border="2px solid blue" margin="20px"></Grid>
-      <Box className="extra-info" border="2px solid blue" margin="20px"></Box>
-      <Grid className="footer" border="2px solid blue" margin="20px"></Grid>
     </GridItem>
   )
   
---
+++ foodinflight_frontend/src/data/Kitchens.js
@@ -0,0 +1,5 @@
+export const KITCHENS = [
+  {title:"Итальянская", url:"italian"},
+  {title:"Американская", url:"american"},
+  {title:"Китайская", url:"chineese"},
+]
