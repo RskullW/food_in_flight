65eb80668bad0185d2e3937c2eec6d5a629d1930
Sergey Balkunov <104581878+itmosha@users.noreply.github.com>
2023-03-15 22:52:27 +0300

    feat: change password view

--- foodinflight_backend/authentication/serializers.py
+++ foodinflight_backend/authentication/serializers.py
@@ -19,3 +19,9 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
         return user
+    
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
