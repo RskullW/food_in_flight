ecd9b171fec5aa1ec24163b14b9bbf415ed11557
Sergey Balkunov <104581878+itmosha@users.noreply.github.com>
2023-03-18 19:31:16 +0300

    feat: custom list handlers for order api

--- foodinflight_backend/main/views.py
+++ foodinflight_backend/main/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
+from knox.models import AuthToken
 from .models import *
 from .serializers import *
+from django.contrib.auth.models import User
 
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
@@ -40,7 +42,7 @@ class OrderViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.action == 'list':
-            permission_classes = [permissions.AllowAny]
+            permission_classes = [permissions.IsAuthenticated]
         elif self.action == 'retrieve':
             permission_classes = [permissions.AllowAny]
         elif self.action == 'create':
@@ -50,6 +52,32 @@ class OrderViewSet(viewsets.ModelViewSet):
         else:
             permission_classes = [permissions.IsAdminUser]
         return [permission() for permission in permission_classes]
+
+    def list(self, request, *args, **kwargs):
+        access_token_provided = request.META.get('HTTP_AUTHORIZATION')
+
+        if access_token_provided is not None:
+            token_key = access_token_provided[6:14]
+            token = AuthToken.objects.filter(token_key=token_key)[:1].get()
+
+            if token:
+                requested_user_id = int(token.user_id)
+
+                requested_user = User.objects.filter(id=requested_user_id)[:1].get()
+                
+                if requested_user.is_staff:
+                    queryset = Order.objects.all()
+                else:
+                    queryset = Order.objects.filter(email=requested_user.username)
+
+                page = self.paginate_queryset(queryset)
+                if page is not None:
+                    serializer = self.get_serializer(page, many=True)
+                    return self.get_paginated_response(serializer.data)
+
+                serializer = self.get_serializer(queryset, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+
 
     def create(self, request, *args, **kwargs):
         items = request.data.pop('items')
