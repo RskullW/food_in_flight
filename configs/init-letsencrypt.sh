52db492b12bca047a4d1c53bae487520a1a9cb93
victor <vi350@inbox.ru>
2023-03-19 04:36:16 +0300

    fix: nginx and init-letsencrypt.sh fixed

--- configs/init-letsencrypt.sh
+++ configs/init-letsencrypt.sh
@@ -5,11 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-export "$(grep '^HOST_DOMAIN' .env | xargs -d '\n')"
-domains=("$HOST_DOMAIN")
+domains=($(grep '^HOST_DOMAIN' ../.env | xargs | cut -d '=' -f 2))
 rsa_key_size=4096
 data_path="../data/certbot"
-email="example@$HOST_DOMAIN"
+email=""
 staging=0
 
 if [ -d "$data_path" ]; then
@@ -31,23 +30,23 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
+docker-compose --env-file ../.env run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
+    -subj '/CN=localhost'" foodinflight_certbot
 echo
 
 
 echo "### Starting nginx ..."
-docker-compose up -f ../docker-compose.yml --force-recreate -d foodinflight_nginx
+docker-compose --env-file ../.env -f ../docker-compose.yml up --force-recreate -d foodinflight_nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose run -f ../docker-compose.yml --rm --entrypoint "\
+docker-compose --env-file ../.env -f ../docker-compose.yml run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" foodinflight_certbot
 echo
 
 
@@ -67,15 +66,15 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run -f ../docker-compose.yml --rm --entrypoint "\
+docker-compose --env-file ../.env -f ../docker-compose.yml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
-    --force-renewal" certbot
+    --force-renewal" foodinflight_certbot
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec -f ../docker-compose.yml foodinflight_nginx nginx -s reload
+docker-compose --env-file ../.env -f ../docker-compose.yml exec foodinflight_nginx nginx -s reload
